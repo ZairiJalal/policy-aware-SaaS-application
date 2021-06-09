c293ae01effc89d3025a4902ca15b047655e0d86
ZairiJalal <zairi.jalal98@gmail.com>
2021-06-09 21:27:46 +0200

    croos

--- src/main/java/org/sid/web/UserRestService.java
+++ src/main/java/org/sid/web/UserRestService.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
   
   @RestController
-  @RequestMapping() 
-
   @CrossOrigin(origins = "*", allowedHeaders = "*")
   
   public class UserRestService {
