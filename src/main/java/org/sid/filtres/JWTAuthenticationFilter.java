84706fe2b9d169e99839b0dc3278a9269c364dc4
ZairiJalal <zairi.jalal98@gmail.com>
2021-06-10 12:04:50 +0200

    test

--- src/main/java/org/sid/filtres/JWTAuthenticationFilter.java
+++ src/main/java/org/sid/filtres/JWTAuthenticationFilter.java
@@ -44,10 +44,12 @@ import
 	  System.out.println("----------------------------------------");
 	  System.out.println(us.getIdTenant());
 	  System.out.println(request.getParameter("idTenant"));
-	  if(us.getIdTenant().equals(request.getParameter("idTenant")))
-		  return   authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(appUser.getUsername(),appUser.getPassword())); 
-	  return null;
-	  }
+		/*
+		 * if(us.getIdTenant().equals(request.getParameter("idTenant")))
+		 */		  return   authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(appUser.getUsername(),appUser.getPassword())); 
+		/*
+		 * return null;
+		 */	  }
   
   @Override protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
 	  System.out.println("----------------- JJT-------------------");
